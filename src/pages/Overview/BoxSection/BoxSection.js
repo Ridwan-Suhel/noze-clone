7f3c668d5bca6b26d386659c60db177ac4836d9d
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-07-19 17:33:28 +0600

    headlines box section added

---
+++ src/pages/Overview/BoxSection/BoxSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BoxSection = () => {
+  return (
+    <section className="py-16 bg-[#F3F2F8]">
+      <div className="container mx-auto px-4 lg:px-16">
+        <h2 className="text-[#210545] text-[48px] leading-[48px] font-medium">
+          NOZE is making
+          <br />
+          headlines.
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default BoxSection;
--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BoxSection from "./BoxSection/BoxSection";
 import Hero from "./Hero/Hero";
 
 const Overview = () => {
   return (
     <main>
       <Hero />
+      <BoxSection />
     </main>
   );
 };
